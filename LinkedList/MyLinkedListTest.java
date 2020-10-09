368f92b1cba820a78568332bb16b079a9dda0bfa
kvishwasrujan <roohithbandi00@gmail.com>
2020-10-09 21:38:10 +0530

    Added UC2_CreateLinkedList

---
+++ LinkedList/MyLinkedListTest.java
@@ -0,0 +1,14 @@
+package com.Capgemini.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyLinkedListTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
