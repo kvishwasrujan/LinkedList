bada4fa778166b34a90dcd1b31e2024db069a3d2
kvishwasrujan <roohithbandi00@gmail.com>
2020-10-09 21:50:55 +0530

    Added Append LinkedList

--- LinkedList/MyLinkedList.java
+++ LinkedList/MyLinkedList.java
@@ -1,41 +1,40 @@
 package com.Capgemini.LinkedList;
 
 public class MyLinkedList {
-		public INode head;
-		public INode tail;
-		
-		public MyLinkedList() {
-			this.head = null;
-			this.tail = null;
-		}
+	public INode head;
+	public INode tail;
+	
+	public MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
 
-		
-		public void printMyNodes() {
-			StringBuffer myNodes = new StringBuffer("My Nodes: ");
-			INode tempNode = head;
-			while(tempNode.getNext()!=null) {
-				myNodes.append(tempNode.getKey());
-				if(!(tempNode.equals(tail))) 
-					myNodes.append("->");
-				tempNode = tempNode.getNext();
-			}
+	
+	public void printMyNodes() {
+		StringBuffer myNodes = new StringBuffer("My Nodes: ");
+		INode tempNode = head;
+		while(tempNode.getNext()!=null) {
 			myNodes.append(tempNode.getKey());
-			System.out.println(myNodes);
+			if(!(tempNode.equals(tail))) 
+				myNodes.append("->");
+			tempNode = tempNode.getNext();
 		}
+		myNodes.append(tempNode.getKey());
+		System.out.println(myNodes);
+	}
 
 
-		public void add(INode newNode) {
-			// TODO Auto-generated method stub
-				if(this.tail == null)
-					this.tail = newNode;
-				if(this.head == null)
-					this.head = newNode;
-				else {
-					INode tempNode = this.head;
-					this.head = newNode;
-					this.head.setNext(tempNode);
-				}
-				}
+	public void add(INode newNode) {
+		// TODO Auto-generated method stub
+		if(this.tail == null)
+			this.tail = newNode ;
+		if(this.head == null)
+			this.head =newNode ;
+		else {
+			this.tail=null;
+			this.tail =newNode ;
+		}
+	}
 		}
 	
 
