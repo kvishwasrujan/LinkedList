674cdf03e51b1e0861c6fad520cfaf14d5ba6d3e
kvishwasrujan <roohithbandi00@gmail.com>
2020-10-13 08:06:03 +0530

    UC3_SearchAValue Functionality Added

--- LinkedList/MyBinaryTreeTest.java
+++ LinkedList/MyBinaryTreeTest.java
@@ -1,11 +1,12 @@
 package com.Capgemini.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import junit.framework.Assert;
-class MyBinaryTreeTest {
 
+class MyBinaryTreeTest {
 	@Test
 	void whenAddedThreeNumbersItShouldReturnSizeThree() {
 		MyBinaryTree<Integer> myBinaryTree=new 	MyBinaryTree<Integer>();
@@ -34,5 +35,23 @@ class MyBinaryTreeTest {
 		int size = myBinaryTree.getSize();
 		assertEquals(13, size);
 	}
+	@Test
+	public void searchForParticularKey() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		MyBinaryNode<Integer> reqNum =myBinaryTree.search(63);
+		assertEquals(63,(int)reqNum.key);
+	}
 }
-
